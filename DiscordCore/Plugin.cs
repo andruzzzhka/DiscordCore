523feb4e7af23c1d587677bad5efcf2565743d01
andruzzzhka <andruzzzhka@yandex.ru>
2020-01-08 16:24:09 +0700

    Add project files.

---
+++ DiscordCore/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using IPA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace DiscordCore
+{
+    public class Plugin : IBeatSaberPlugin
+    {
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+
+        }
+
+        public void OnApplicationQuit()
+        {
+
+        }
+
+        public void OnApplicationStart()
+        {
+
+        }
+
+        public void OnFixedUpdate()
+        {
+
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+    }
+}
